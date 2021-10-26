e0ca3f589236c3d235ef04a7416c4c83980ebcc4
Jim <vucuongJim@gmail.com>
2021-10-26 17:55:04 +0700

    Change sqlite to mysql database. Add docker-compose file for running mysql docker image. Add packages to connect to mysql

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"jim/twitter/pkg/server"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(sqlite.Open("twitter.db"), &gorm.Config{})
+	// _, err := gorm.Open(sqlite.Open("twitter.db"), &gorm.Config{})
+	dsn := "user:123@tcp(0.0.0.0:3306)/twitter?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	} else {
